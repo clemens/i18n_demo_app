747c07e9afb0d0153884ddc71f882bc10ee0f3c3
Clemens Kofler <clemens@railway.at>
2008-08-28 12:32:56 +0200

    Cleanup of source code display.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  
+  def show_source(file)
+    output = ''
+    counter, lineWidth = 1, 80
+    while(line = @locale_file.gets)
+      output << "#{sprintf('%3d', counter)}: #{line}"
+      counter += 1
+    end
+    output
+  end
 end
