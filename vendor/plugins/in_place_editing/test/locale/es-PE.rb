4e07c995f8aa432092454e8d94ecdf0ae6564e49
Clemens Kofler <clemens@railway.at>
2008-08-28 11:09:53 +0200

    Removed in place editing stuff - not part of Rails core, therefore not in the scope of demo app.

--- vendor/plugins/in_place_editing/test/locale/es-PE.rb
+++
@@ -1,9 +0,0 @@
-I18n.store_translations 'es-PE',
-  :in_place_editing => {
-    :text => {
-      :cancel => "cancelar",
-      :loading => "cargando...",
-      :save => "guardar",
-      :saving => "guardando..."
-    }
-  }
