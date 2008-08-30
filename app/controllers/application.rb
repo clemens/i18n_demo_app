3ede1b2a59562002ef75bd6d64cbc273b5968e7f
Clemens Kofler <clemens@railway.at>
2008-08-30 21:20:34 +0200

    Implemented support for both, rb and yml translation files.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,23 +7,25 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'fc86fd4bfda299c3b079191a8f948013'
-  
+
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
-  
+
   before_filter :set_locale
-    
+
   def set_locale
-    
-    #update session if passed
+    # update session if passed
     session[:locale] = params[:locale] if params[:locale]
-    
-    #set locale based on session or default 
+
+    # set locale based on session or default 
     I18n.locale = session[:locale] || I18n.default_locale
-    
-    #load locale from settings
-    I18n.load_translations "#{LOCALES_DIRECTORY}#{I18n.locale}.rb"
+
+    # load locale from settings
+    ['yml', 'rb'].each do |type|
+      locale_file = "#{LOCALES_DIRECTORY}#{I18n.locale}.#{type}"
+      I18n.load_translations locale_file if File.exists?(locale_file)
+    end
   end
 end
