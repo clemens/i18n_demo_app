9fb79a447b6eac85dfc498ddd9450049f13f21ba
Karel Minarik <karmi@karmi.cz>
2008-09-02 18:45:46 +0200

    Rubyfied loading locales in "localized_dates"

--- vendor/plugins/localized_dates/init.rb
+++ vendor/plugins/localized_dates/init.rb
@@ -5,9 +5,8 @@ locales_dir = File.join(RAILS_ROOT, 'config', 'locales')
 
 #TODO: clean up so that only responds to current locale - load order dependent.
 # load all ruby and YML hash locales
-Dir["#{locales_dir}/*.rb", "#{locales_dir}/*.yml"].uniq.each do |locale|
-  I18n.load_translations locale
-end
+Dir["#{locales_dir}/*.{rb,yml}"].each { |locale| I18n.load_translations locale }
+
 puts '** [localized_dates] locales loaded from config/locales.'
 
 # set default locale to en-US
