cd134311d4d72e36df1630c118704bffa988c4e8
Clemens Kofler <clemens@railway.at>
2008-09-11 15:27:26 +0200

    Sort available locales.

--- config/environment.rb
+++ config/environment.rb
@@ -71,4 +71,4 @@ I18n.default_locale = 'en-US'
 LOCALES_DIRECTORY = "#{RAILS_ROOT}/config/locales/"
 LOCALES_AVAILABLE = Dir["#{LOCALES_DIRECTORY}/*.{rb,yml}"].collect do |locale_file|
   File.basename(File.basename(locale_file, ".rb"), ".yml")
-end.uniq
+end.uniq.sort
