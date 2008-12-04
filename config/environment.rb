0689d942921b8117c245ee6dddc29238957d4ffc
Akira Matsuda <ronnie@dio.jp>
2008-12-05 05:56:27 +0900

    Removed redundant '/' from LOCALES_DIRECTORY

--- config/environment.rb
+++ config/environment.rb
@@ -68,7 +68,7 @@ end
 
 I18n.default_locale = 'en'
 
-LOCALES_DIRECTORY = "#{RAILS_ROOT}/config/locales/"
+LOCALES_DIRECTORY = "#{RAILS_ROOT}/config/locales"
 LOCALES_AVAILABLE = Dir["#{LOCALES_DIRECTORY}/*.{rb,yml}"].collect do |locale_file|
   I18n.load_path << locale_file
   File.basename(File.basename(locale_file, ".rb"), ".yml")
